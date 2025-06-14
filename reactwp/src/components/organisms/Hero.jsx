5e37f86fb7733f7779c087e55fb06ee1825732a5
Juan David González <jd-gonzalez@javeriana.edu.co>
2025-06-13 19:50:29 -0500

    Feature: Remove emoji from Hero title and update TextImage description

--- reactwp/src/components/organisms/Hero.jsx
+++ reactwp/src/components/organisms/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="bg-gradient-to-r from-blue-500 to-white py-3 px-6 heigh-auto">
       <div className="max-w-5xl mx-auto text-center">
         <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-3">
-          Bienvenido a mi página web 🚀
+          Bienvenido a mi página web
         </h1>
         <p className="text-gray-600 text-lg md:text-xl mb-3">
           En esta página puedes revisar mi información personal.
